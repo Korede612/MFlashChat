d338538d6ca822e30b46305413bbb322cb809c60
Korede612 <korede.oko-osi@nytengaletech.com>
2024-03-18 07:10:36 +0100

    Initial Commit

---
+++ MFlashChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MFlashChat
+//
+//  Created by Oko-osi Korede on 18/03/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
